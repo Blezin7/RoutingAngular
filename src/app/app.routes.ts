2dbbb0a26a6f5f2c8cb35a73a7fb1c1caead1222
Blezin7 <rblessing555@gmail.com>
2024-10-20 20:48:59 +0530

    just routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Comp1Component } from './comp1/comp1.component';
+import { Comp2Component } from './comp2/comp2.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'Comp1', component: Comp1Component},
+    {path: 'Comp2', component: Comp2Component},
+
+];
